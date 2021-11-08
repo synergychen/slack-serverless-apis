0e4ae5b998a5c517503f8de7403b72320928cfd2
Chen Huang <alan.tolearn@gmail.com>
2021-11-07 20:28:27 -0500

    Add slack event api

--- handler.js
+++ handler.js
@@ -1,18 +1,43 @@
-'use strict';
+// Verify Url - https://api.slack.com/events/url_verification
+function verify(data, callback) {
+  console.log(`token: ${data.token}`)
+  if (data.token === process.env.VERIFICATION_TOKEN)
+    callback(null, data.challenge)
+  else callback('verification failed')
+}
 
-module.exports.hello = async (event) => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify(
-      {
-        message: 'Go Serverless v1.0! Your function executed successfully!',
-        input: event,
-      },
-      null,
-      2
-    ),
-  };
+// Post message to Slack - https://api.slack.com/methods/chat.postMessage
+function eventHandler(event, callback) {
+  // test the message for a match and not a bot
+  if (!event.bot_id && /(aws|lambda)/gi.test(event.text)) {
+    const text = `<@${event.user}> isn't AWS Lambda awesome?`
+    postToSlack(text)
+  }
 
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
-};
+  callback(null)
+}
+
+function postToSlack(message) {
+  const postData = {
+    text: message
+  }
+  console.log(postData)
+}
+
+module.exports.slackEvents = async (event, context, callback) => {
+  try {
+    const data = JSON.parse(event.body)
+    switch (data.type) {
+      case 'url_verification':
+        verify(data, callback)
+        break
+      case 'event_callback':
+        eventHandler(data.event, callback)
+        break
+      default:
+        callback(null)
+    }
+  } catch (e) {
+    console.log(e)
+  }
+}

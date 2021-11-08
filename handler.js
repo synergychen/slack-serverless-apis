da46f8fc7f47e23cdd25d80cf68d0c3cde3c3104
Chen Huang <alan.tolearn@gmail.com>
2021-11-07 21:55:33 -0500

    Add slack interactive action api

--- handler.js
+++ handler.js
@@ -43,3 +43,40 @@ module.exports.handleFarmingResultsEvents = async (event, context, callback) => 
     console.log(e)
   }
 }
+
+module.exports.handleFarmingResultsInteractiveActions = async (event) => {
+  const payloadStr = decodeURIComponent(event.body).replace(/\+/g, ' ')
+  const payload = JSON.parse(payloadStr.replace('payload=', ''))
+  const reply = payload.original_message
+  let attachment
+
+  if (payload.actions[0].value === 'yes') {
+    attachment = {
+      text: 'Item will be kept',
+      color: '#36a64f'
+    }
+  } else if (payload.actions[0].value === 'no') {
+    attachment = {
+      text: 'Item will be ignored',
+      color: '#e5486e'
+    }
+  } else {
+    attachment = {
+      text: 'Invalid input',
+      color: '#e5486e'
+    }
+  }
+  // Remove buttons from original message
+  delete reply.attachments[reply.attachments.length - 1].actions
+  // Append result to original message
+  reply.attachments.push(attachment)
+
+  console.log(reply)
+
+  const response = {
+    statusCode: 200,
+    body: JSON.stringify(reply)
+  }
+  return response
+}
+
